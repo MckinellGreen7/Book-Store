2524b93c42b6a18044fb02202142f2ebc52f4f1f
MckinellGreen7 <khandelwalshriyansh0412@gmail.com>
2024-05-22 10:00:37 +0530

    changed cors

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express()
 app.use(express.json())
 // app.use(cors())
 app.use(cors({
-    origin: "http://localhost:5173",
     methods: ['GET','POST','PUT','DELETE'],
     allowedHeaders: ['Content-Type']
 }))
